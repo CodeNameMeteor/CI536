2c9ec87dbcb9dbcdcb03697303ec20e1f3265d53
CodeNameMeteor <dylanpsilby@gmail.com>
2026-05-07 12:42:15 +0100

    small

--- src/main/java/org/CI536/analyser/ui/PacketTableView.java
+++ src/main/java/org/CI536/analyser/ui/PacketTableView.java
@@ -43,7 +43,7 @@ public class PacketTableView extends Application {
 
     @Override
     public void start(Stage stage) {
-        stage.setTitle("Java Network Analyzer");
+        stage.setTitle("Light Sniff");
         stage.setWidth(1200);
         stage.setHeight(600);
 
